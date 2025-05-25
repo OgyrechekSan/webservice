8f4f2ec563d845f48562c8a8ae3cde1b81c330fc
OgyrechekSan <balybin.1988@mail.ru>
2025-05-25 23:27:09 +0300

    CI/CD

--- src/main/java/com/example/webservice/services/UserService.java
+++ src/main/java/com/example/webservice/services/UserService.java
@@ -36,7 +36,7 @@ public class UserService {
         }
         user.setActive(true);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.getRoles().add(Role.ROLE_ADMIN);
+        user.getRoles().add(Role.ROLE_USER);
 
         log.info("Saving new User with email: {}", user.getEmail());
         userRepository.save(user);
